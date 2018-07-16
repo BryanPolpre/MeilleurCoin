e646fcbdc5b4068f44fab8ebe459708387eaabc6
Antoine MERCIER <mercier.antoine1@gmail.com>
2018-07-16 14:46:50 +0200

    FAQ CGU

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -13,4 +13,14 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+    
+    public function faqAction(Request $request)
+    {
+        return $this->render('@Site/default/faq.html.twig');
+    }
+    
+    public function cguAction(Request $request)
+    {
+        return $this->render('@Site/default/cgu.html.twig');
+    }
 }
