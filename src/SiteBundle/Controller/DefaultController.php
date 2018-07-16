004f20e457bf37c4c0ef4540fe1139c7b7d86d92
Bastien PoP <Bastien-poupelin@hotmail.fr>
2018-07-16 14:49:00 +0200

    1st vue

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -2,15 +2,12 @@
 
 namespace SiteBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('@Site/Default/index.html.twig');
     }
 }
