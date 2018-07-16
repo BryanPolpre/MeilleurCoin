919392768c1b99197984a57241eb96100b4e5978
Antoine MERCIER <mercier.antoine1@gmail.com>
2018-07-16 17:10:39 +0200

    Debut detail Ad
    Ceci est une description

--- src/SiteBundle/Controller/AdController.php
+++ src/SiteBundle/Controller/AdController.php
@@ -47,4 +47,16 @@ class AdController extends Controller
             'title'=>isset($adForm)?'Test de formulaire pour'.$adForm->getTitle() : 'Test de formulaire',
         );
     }
+    
+    public function detailAction(Request $request){
+        /*$adRepo = $this->getDoctrine()->getRepository("SiteBundle:Ad");
+        $adForm= new Ad();
+        $form->handleRequest($request);*/
+
+        $em = $this->getDoctrine()->getManager();
+        $adRepo = $em->getRepository('SiteBundle:Ad');
+        $this->$ads = $adRepo->findAll();
+
+        return $this->render('@Site/default/detailAd.html.twig', $ads);
+    }
 }
--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -11,12 +11,12 @@ class DefaultController extends Controller
         return $this->render('@Site/Default/index.html.twig');
     }
     
-    public function faqAction(Request $request)
+    public function faqAction()
     {
         return $this->render('@Site/default/faq.html.twig');
     }
     
-    public function cguAction(Request $request)
+    public function cguAction()
     {
         return $this->render('@Site/default/cgu.html.twig');
     }
