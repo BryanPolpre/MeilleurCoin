c95f96b5df240f63683df695e59e35a07930833e
Bryan Polpré <bryanpolpre@outlook.fr>
2018-07-16 12:20:29 +0200

    NGOOOLOOO KANTE

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -2,12 +2,20 @@
 
 namespace SiteBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
     {
-        return $this->render('SiteBundle:Default:index.html.twig');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
