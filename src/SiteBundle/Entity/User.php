caf11056316b3edb3a768747a41669ae0205f791
Bryan Polpré <bryanpolpre@outlook.fr>
2018-07-18 15:00:03 +0200

    login

--- src/SiteBundle/Entity/User.php
+++ src/SiteBundle/Entity/User.php
@@ -3,6 +3,7 @@
 namespace SiteBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 
 /**
@@ -11,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="SiteBundle\Repository\UserRepository")
  */
-class User {
+class User implements UserInterface {
     
     /**
      *
@@ -37,7 +38,7 @@ class User {
     private $password;
         
     /**
-     * @ORM\ManyToMany(targetEntity="Role", cascade={"persist"})
+     * @ORM\Column(type="array")
      */
     private $roles;
         
@@ -45,8 +46,12 @@ class User {
      * @ORM\Column(name="date_registered", type="datetime")
      */
     private $dateRegistered;
-    
-    
+
+    public function __construct()
+    {
+        $this->roles = ['ROLE_USER'];
+    }
+
     public function getId() {
         return $this->id;
     }
@@ -101,4 +106,61 @@ class User {
         return $this;
     }
 
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+        ));
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized, array('allowed_classes' => false));
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        return null;
+    }
 }
